e061cb4fdb51b4d3b4c72f6bfee360398dc37c97
Amaan Nawab <nawab.mohdamaan13@gmail.com>
2020-09-28 15:14:16 +0530

    Some changes

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const {findOrCreateUser } = require('./controllers/userController');
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const { findOrCreateUser } = require("./controllers/userController");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser : true ,  useUnifiedTopology: true})
-.then(()=> console.log('DB is Connected!'))
-.catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected!"))
+  .catch(err => console.error(err));
 
-
-const server =new ApolloServer ({
-    typeDefs ,
-    resolvers,
-    context : async ({req}) => {
-        let authToken = null;
-        let currentUser = null;
-        try{
-           authToken = req.headers.authorization;
-         if(authToken)
-         {
-            currentUser =await findOrCreateUser(authToken);
-           
-         }
-        }catch(err){
-            console.error(`Unable to authenticate the user with Token ${authToken}`);
-        }
-        return {currentUser};
-
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
     }
-})
+    return { currentUser };
+  }
+});
 
-server.listen( {port :  process.env.PORT || 4000 }).then( ({url})=> {
- console.log(`Server is listening on ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
 });
